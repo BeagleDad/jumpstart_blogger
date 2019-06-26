1a4df5884c1e608578672da9aadbacce03738bd0
Matt Hoffman <formatt.hoffman@gmail.com>
2019-06-26 12:40:36 -0700

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+end
