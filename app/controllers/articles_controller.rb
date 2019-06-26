301c686dda5a48a64ef29eb06ae21b8450a14826
Matt Hoffman <formatt.hoffman@gmail.com>
2019-06-26 15:34:17 -0700

    just added delete going to add edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+include ArticlesHelper
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
@@ -7,6 +8,19 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    Article.destroy(@article.id)
+    redirect_to articles_path
   end
 
   def show
